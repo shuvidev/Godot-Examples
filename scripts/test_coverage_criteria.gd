7a3d0ec6ca36bb8b1bef1fa28a2870e37d86c383
SHUVI <shuvi.dev@proton.me>
2024-12-09 14:14:25 -0300

    adds _on_child_update method
    adds a signal to whenever a change happens on the root node, adding or removing a node from it, it will trigger a signal calling _on_child_update method

---
+++ scripts/test_coverage_criteria.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Test Coverage Criteria Initialized")
+	var nodes: Node = get_node('/root')
+	nodes.child_entered_tree.connect(_on_child_update)
+	nodes.child_exiting_tree.connect(_on_child_update)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+# Called whenever a node is updated
+func _on_child_update(node: Node):
+	print("tree updated on node:")
+	print(node)
+	print("=====================")
